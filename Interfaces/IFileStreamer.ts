90141802fe75fbecb777cdd4196702e3fc153f53
Alex Chaliy <alex.chaliy.42@gmail.com>
2017-05-08 18:52:45 +0300

    modified IFileStreamer

--- Interfaces/IFileStreamer.ts
+++ Interfaces/IFileStreamer.ts
@@ -1,6 +1,22 @@
 export interface IFileStreamer {
 	isThereFileChunks():boolean;
-	stopFileReading():number;
+
+	stopFileReading():number; // returns status code
+
+	readChunk():number;
+
+	packChunk():number;
+
+	SendChunkToClient():number;
+
+	// -client- Recieve File Chunk
+	// -client- Parse File Chunk
+	// -client- Is There Track File in Cache ?
+	// -client- Create Empty File in Cache for Current Track
+	// -client- Add Chunk To Track File in Cache
+
+
+
 
 	readFileByChunks():void;
 }
